8e00fe2b06313466cb5757cb6d4db3d4caeecd9c
Robbie Jones <robbie.jones@forgerock.com>
2019-03-25 14:26:26 -0700

    first commit

---
+++ app/src/main/java/com/forgerock/iotdna/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.forgerock.iotdna
+
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.view.Menu
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.FirebaseDatabase
+//import jdk.nashorn.internal.objects.NativeDate.getTime
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+import com.forgerock.iotdna.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var editText: EditText = findViewById(R.id.usr) as EditText
+
+        fab.setOnClickListener { view ->
+            val usr = (editText.text.toString()) /* value can b updated via UI ie USER <> 'DEMO' */
+
+            // DO NOT TEST ON EMULATOR!!! SINCE IT WONT REACH OUT TO FIREBASE
+
+            FirebaseApp.initializeApp(this);
+            val database = FirebaseDatabase.getInstance()// Write a message to the database
+            val topic_name = database.getReference(usr) // remember this is really just firebaseurl + "/" + (usr).json
+
+            topic_name.setValue(Build.SERIAL + "^" + getTime())   // update the file
+            Snackbar.make(view, "iotDna challenge sent " + usr + "@" + Build.SERIAL, Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+    }
+
+    private fun getTime(): String {
+        var date: Date? = null
+        try {
+            val stamp = Timestamp(System.currentTimeMillis())
+            date = Date(stamp.getTime())
+            val sdf = SimpleDateFormat("MM/dd/yyyy h:mm:ss a")
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"))
+            val formattedDate = sdf.format(date)
+            //System.out.println(date);
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return date!!.toString()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+}
